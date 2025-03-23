ce1b9c516f58398659edfb717ffcb917979c2abe
Falin Denis <falin_d@mail.ru>
2025-03-23 21:54:32 +0300

    feat(task-2): change FileExceptionHandlerLog

--- local/php-interface/init.php
+++ local/php-interface/init.php
@@ -1,10 +1,8 @@
 <?php
  if (file_exists (__DIR__ . '/../../vendor/autoload.php')) {
   require_once __DIR__ . '/../../vendor/autoload.php';
-
+}
+/*var_dump(__DIR__ . '/src/autoloader.php');*/
+ if (file_exists (__DIR__ . '/src/autoloader.php')) {
+    require_once __DIR__ . '/src/autoloader.php';
  }
-
- dump([
-  'жена' => '+77777777',
-  'мама' => '+88888888'
- ]);
